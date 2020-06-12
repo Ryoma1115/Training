7bb5939e8df27b92fe6608c09cbd098e39b8f490
vagrant <vagrant@localhost.localdomain>
2020-06-12 14:38:10 +0000

    finish chapter1

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,12 @@ module Training
     config.load_defaults 5.2
 
     config.generators do |g|
-      g.assets false          # CSS, JavaScriptファイルは生成しない
+      g.assets false # CSS, JavaScriptファイルは生成しない
       g.test_framework :rspec,
-        controller_specs: false,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false
+                       controller_specs: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
