7ca4c0314c8d4e2533055d0ccf318f3a7d3f3267
vagrant <vagrant@localhost.localdomain>
2020-06-13 13:30:00 +0000

    メールアドレスの一意性を強制するためカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_132256) do
+ActiveRecord::Schema.define(version: 2020_06_13_132728) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_06_13_132256) do
     t.string "sex"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
